fd0b78cf19f7104d2b48144af73ec872a475e86b
Aly <el7amrawy@outlook.com>
2022-09-18 12:09:34 +0200

    fixed start page

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -3,7 +3,7 @@ const Start = (props) => {
     <section className="start">
       <div>
         <h2>Quizzical</h2>
-        <h4>Some description if needed</h4>
+        {/* <h4>Some description if needed</h4> */}
         <button onClick={() => props.setStart(false)}>Start quiz</button>
       </div>
     </section>
